e5d98fa2c82f6d8c364b1c4a960956c06402f231
Jack <jakpatel1817@gmail.com>
2024-03-22 17:01:37 +0530

    commit

--- lib/controller/leaves_controller.dart
+++ lib/controller/leaves_controller.dart
@@ -1,3 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
-class LevesController extends GetxController {}
+class LeavesController extends GetxController {
+  Widget container(
+      {required String text1,
+      required String text2,
+      required String text3,
+      Color? boxColor,
+      BoxBorder? border,
+        TextStyle? style
+      }) {
+    return Container(
+      height: 130,
+      width: double.maxFinite,
+      margin: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: boxColor,
+        border: border,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              text1,
+              style: const TextStyle(fontWeight: FontWeight.w900),
+            ),
+            Expanded(
+                child: Text(
+              text2,
+              style: const TextStyle(fontWeight: FontWeight.w900),
+            )),
+            Text(text3,
+              style: style,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
